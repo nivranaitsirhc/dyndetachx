73b596727a7eee55212390c4c93c38b2e312b7e4
nivranaitsirhc <naitsirhc.uriel@gmail.com>
2023-05-02 13:03:52 +0800

    revert to Deflate from LZMA compression

--- build.cmd
+++ build.cmd
@@ -12,7 +12,17 @@ if not defined version (
 	goto:exit
 )
 
-powershell Compress-Archive -Path .\src\* -DestinationPath .\dyndetachx_%version%.zip -Force -CompressionLevel Fastest 
+set zip="C:\Program Files\7-Zip\7z.exe"
+
+if not exist %zip% (
+	echo "No Zip File"
+	goto:exit
+)
+%zip% a -tzip -mx=9 -mm=Deflate -mcu=on -mmt=on -r .\dyndetachx_%version%.zip .\src\*
+
+::powershell Compress-Archive -Path .\src\* -DestinationPath .\dyndetachx_%version%.zip -Force -CompressionLevel Fastest 
+
+
 
 :exit
 endlocal
