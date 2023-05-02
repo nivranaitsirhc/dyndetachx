8794d8739dec9fb94b00574aba72ab8d6c5dba57
nivranaitsirhc <naitsirhc.uriel@gmail.com>
2023-05-02 21:20:11 +0800

    start playstore after detaching, cleanup script

--- src/detach.sh
+++ src/detach.sh
@@ -1,22 +1,16 @@
 #!/system/bin/sh
 # Dynamic Detached Module for ReVanced Assist Module
-
+#
 # magisk_module required
 MODDIR="${0%/*}"
-MODNAME="${MODDIR##*/}"
-
+#MODNAME="${MODDIR##*/}"
 # config-paths
 # ------------
-# magisk Busybox
-export PATH="$MAGISKTMP/.magisk/busybox:$PATH"
-# module local binaries
-export PATH="$MODDIR/bin:$PATH"
-
+# Module & Magisk Busybox Binaries
+export PATH="$MODDIR/bin:$MAGISKTMP/.magisk/busybox:$PATH"
 # config-static_variables 
 # -----------------------
-# path_detach_file_magisk="/data/adb/detach.sh"
 path_detach_file_module="$MODDIR/detach.txt"
-# path_detach_file="/data/local/tmp/detach.sh"
 # -----------------------
 PS=com.android.vending
 DB=/data/data/$PS/databases
@@ -25,19 +19,16 @@ LADB=$DB/localappstate.db
 toggled_playstore_disabled=false
 toggled_foced_detach=false
 detached_list=""
-
-#
+# Internal Storage folder
 sdcard_folder=/sdcard/DynamicDetachX
-
 # check for db files
 { [ ! -f "$LDB" ] || [ ! -f "$LADB" ]; } && {
 	exit 1
 }
-
+# send notifications
 send_notification() {
     su 2000 -c "cmd notification post -S bigtext -t 'DynDetachX' 'Tag' '$(printf "$1")'"
 }
-
 # tag files
 [ -f "$sdcard_folder/enable" ] && {
 	[ -f "$sdcard_folder/detach.txt" ] && {
@@ -61,12 +52,10 @@ send_notification() {
 		rm -rf "$sdcard_folder/force"
 	}
 }
-
 # check for detach file
 [ ! -f "$path_detach_file_module" ] && {
 	exit 1
 }
-
 # main-process
 # ------------
 while IFS= read -r package_name || [ -n "$package_name" ];do
@@ -91,9 +80,10 @@ while IFS= read -r package_name || [ -n "$package_name" ];do
 		detached_list="$detached_list\n$package_name"
 	}
 done < "$path_detach_file_module"
-
 # clear playstore cache
 [ $toggled_playstore_disabled = true ] && {
 	send_notification "Detached Apps: $detached_list"
 	rm -rf /data/data/$PS/cache/*
+	# start playstore
+	am start -n "$(cmd package resolve-activity --brief $PS | tail -n 1)"
 }
